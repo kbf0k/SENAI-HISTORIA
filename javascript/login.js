b438eb37313f14a255cc329210cec3f0ac590be8
Kaique Ferreira <kaique1245br@gmail.com>
2024-10-16 23:56:06 -0300

    Login,cadastro e redefinir senha finalizado com o html,css e js

--- javascript/login.js
+++ javascript/login.js
@@ -1,1 +1,14 @@
+var criar_conta = document.getElementById('criar-conta');
+var voltar = document.getElementById('voltar');
+var login = document.getElementById('login');
+var cadastrar = document.getElementById('cadastrar');
 
+criar_conta.addEventListener('click', () => {
+    login.style.left = "-450px"
+    cadastrar.style.left = "0"
+})
+
+voltar.addEventListener('click', () => {
+    cadastrar.style.left = "900px"
+    login.style.left = "40px"
+})
