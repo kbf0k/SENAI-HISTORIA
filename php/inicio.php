34d5b3b1d2f98072197cc197ab9df1e8432483aa
YagoRGM <yago.roberto2008@gmail.com>
2024-10-22 10:05:17 -0300

    Idade Contemporânea Finalizada 100%

--- php/inicio.php
+++ php/inicio.php
@@ -26,7 +26,7 @@ if(!isset($_SESSION['nome_sessao'])){
     <header class="header">
         <div class="container logo-menu">
             <div class="logo">
-                <img src="../img/LOGO-HISTORIA-INICIO-SEM-FUNDO.png" alt="Logo">
+                <a href="inicio.php"><img src="../img/LOGO-HISTORIA-INICIO-SEM-FUNDO.png" alt="Logo"></a>
                 <h1>História</h1>
             </div>
             <nav class="menu">
@@ -35,7 +35,7 @@ if(!isset($_SESSION['nome_sessao'])){
                         <a href="">Períodos Históricos</a>
 
                         <div class="dropdown-menu">
-                            <a href="">Pré-História</a>
+                            <a href="pre_historia.php">Pré-História</a>
                             <a href="idade_antiga.php">Idade Antiga</a>
                             <a href="idade_media.php">Idade Média</a>
                             <a href="idade_moderna.php">Idade Moderna</a>
