889a58a9364dbb227b9eef2644918ed70d681606
YagoRGM <yago.roberto2008@gmail.com>
2024-11-07 16:21:20 -0300

    yago

--- javascript/inicio.js
+++ javascript/inicio.js
@@ -45,3 +45,34 @@ function toggleMenu() {
     document.querySelector('.menu').classList.toggle('show');
 }
 
+
+// MENUMOBILE
+
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
+
+document.getElementById('btn-mobile').addEventListener('click', function() {
+    const nav = document.getElementById('nav');
+    const btn = document.getElementById('btn-mobile');
+    
+    nav.classList.toggle('active');
+    
+    // Atualizar o aria-expanded para acessibilidade
+    const isExpanded = nav.classList.contains('active');
+    btn.setAttribute('aria-expanded', isExpanded ? 'true' : 'false');
+});
--- php/inicio.php
+++ php/inicio.php
@@ -62,6 +62,39 @@ if(!isset($_SESSION['nome_sessao'])){
             </nav>
         </div>
     </header>
+    <div id="mobile-header" >
+        <button aria-label="Abrir Menu" id="btn-mobile" aria-haspopup="true" aria-controls="menu" aria-expanded="false">
+            <span id="hamburger"></span>
+        </button>
+        <a id="mobile-logo" href="inicio.php"><img src="../img/Logo Nova Site História.png" class="logo" alt=""></a>
+        <div class="user-options">
+            <div class="user-vector">
+                <?php if (isset($_SESSION['nome_sessao'])): ?>
+                    <img id="logo-vector" src="../img/user-vector.png" alt="Imagem do usuário">
+                    <div class="user-info">
+                        <p><?= $_SESSION['nome_sessao'] ?></p>
+                        <p><?= $_SESSION['tipo_sessao'] ?></p>
+                    </div>
+                <?php endif; ?>
+            </div>
+            <?php if (isset($_SESSION['nome_sessao'])): ?>
+                <a id="logout">SAIR</a>
+            <?php else: ?>
+                <a href="index.php">LOGIN</a>
+            <?php endif; ?>
+        </div>
+        <nav id="nav">
+            <ul id="mobile-menu" role="mobile-menu">
+                <li><a href="pre_historia.php">Pré-História</a></li>
+                <li><a href="idade_antiga.php">Idade Antiga</a></li>
+                <li><a href="idade_media.php">Idade Média</a></li>
+                <li><a href="idade_moderna.php">Idade Moderna</a></li>
+                <li><a href="idade_contemporanea.php">Idade Contemporânea</a></li></li>
+                <li><a href="glossario.php">Glossário</a></li>
+                <li><a href="atividades.php">Atividades Complementares</a></li>
+            </ul>
+        </nav>
+    </div>
 
     <main>
         <?php ?>
